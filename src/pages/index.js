c155c33be54e082b8eebf81d17306d653fa1d7ef
Mark Mac <mark.boribor73@gmail.com>
2022-09-13 20:45:08 +0800

    tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Logo from '@site/static/img/logo.jpg';
 
 import styles from './index.module.css';
 
@@ -13,7 +14,8 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <img src={Logo} width="200" style={{borderRadius: '10px', marginBottom: '15px'}} alt={`Mark's Logo`} />            
+        <p className="hero__subtitle"><em>&ldquo;{siteConfig.tagline}&rdquo;</em></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
